25d158ad428c9a55e444a79a937f76b4f2ad935f
Brendon Brewer <brendon.brewer@gmail.com>
2016-07-02 15:31:31 +1200

    We need a classic nested sampler first

---
+++ code/ClassicNestedSampler.h
@@ -0,0 +1,41 @@
+#ifndef LiNeS_ClassicNestedSampler
+#define LiNeS_ClassicNestedSampler
+
+#include <vector>
+#include "DNest4/code/RNG.h"
+
+namespace LiNeS
+{
+
+/*
+* An object of this class is a classic nested sampler.
+*/
+template<class ModelType>
+class ClassicNestedSampler
+{
+    private:
+        // A random number generator to use.
+        DNest4::RNG rng;
+
+        // The particles
+        std::vector<ModelType> particles;
+
+    public:
+        /*
+        * Constructor arguments:
+        *   size_t num_particles = the number of particles
+        *   unsigned int seed = random number generator seed
+        */
+        ClassicNestedSampler(std::size_t num_particles, unsigned int seed = 0);
+
+        /*
+        * Initialise the particles by generating them from the prior.
+        */
+};
+
+} // namespace LiNeS
+
+#include "ClassicNestedSamplerImpl.h"
+
+#endif
+
---
+++ code/ClassicNestedSamplerImpl.h
@@ -0,0 +1,5 @@
+namespace LiNeS
+{
+
+} // namespace LiNeS
+
