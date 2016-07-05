32128e30f3657c72c13c9744a3fbc45e419f9a30
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-07-05 12:36:28 +1200

    Shitty logZ estimate

--- code/showresults.py
+++ code/showresults.py
@@ -21,4 +21,24 @@ plt.show()
 
 # Load the particles
 particles_info = dn4.my_loadtxt("particles_info.txt")
+run_id = particles_info[:,0].astype("int64")
+level_id = particles_info[:,1].astype("int64")
+
+# Prior weights
+logw = np.empty(particles_info.shape[0])
+
+# Loop over runs
+for i in range(0, np.max(run_id) + 1):
+    # Find particles from this run
+    which = (run_id == i)
+    _particles_info = particles_info[which, :]
+    logw[which] = levels_logX[level_id[which]]
+
+logw = logw - dn4.logsumexp(logw)
+logp = logw + particles_info[:,2]
+logZ = dn4.logsumexp(logp)
+print(logZ)
+
+plt.plot(np.exp(logp - logZ), "k")
+plt.show()
 
