c1c4282e56a0f65809ea8f6e74ac5a576609176f
Brendon Brewer <brendon.brewer@gmail.com>
2016-07-07 15:56:30 +1200

    Save posterior sample

--- code/showresults.py
+++ code/showresults.py
@@ -1,8 +1,9 @@
 import numpy as np
+import numpy.random as rng
 import matplotlib.pyplot as plt
 import dnest4.classic as dn4
 
-def postprocess(plot=True, verbose=True):
+def postprocess(plot=True, verbose=True, single_precision=False):
     # Load and plot the levels
     levels_logL = dn4.my_loadtxt("levels_logL.txt").flatten()
     levels_logX = dn4.my_loadtxt("levels_logX.txt")
@@ -22,7 +23,7 @@ def postprocess(plot=True, verbose=True):
     logL = particles_info[:,2]
 
     # Remove particles corresponding to run whose info isn't available yet
-    which = (run_id < levels_logX.shape[0])
+    which = np.nonzero(run_id < levels_logX.shape[0])[0]
     run_id, level_id, logL = run_id[which], level_id[which], logL[which]
 
     # Prior weights
@@ -37,6 +38,33 @@ def postprocess(plot=True, verbose=True):
     logP = logp - logZ
     H = np.sum(np.exp(logP)*(logP - logw))
     ESS = int(np.exp(-np.sum(np.exp(logP)*logP)))
+
+    # Resample to uniform weight
+    W = np.exp(logp - np.max(logp))
+    rows = np.empty(ESS, dtype="int64")
+    for i in range(0, ESS):
+        while True:
+            ii = which[rng.randint(len(which))]
+            if np.random.rand() <= W[ii]:
+                break
+        rows[i] = ii
+
+    sample = dn4.loadtxt_rows("particles.txt", set(rows), single_precision)
+    posterior_sample = None
+
+    if single_precision:
+        posterior_sample = np.empty((ESS, sample["ncol"]), dtype="float32")
+    else:
+        posterior_sample = np.empty((ESS, sample["ncol"]))
+
+    for i in range(0, ESS):
+        posterior_sample[i, :] = sample[rows[i]]
+
+    if single_precision:
+        np.savetxt("posterior_sample.txt", posterior_sample, fmt="%.7e")
+    else:
+        np.savetxt("posterior_sample.txt", posterior_sample)
+    np.savetxt("weights.txt", W)
 
     if verbose:
         print("ln(Z) =", logZ)
